046909869611ef4ce31f1726baf7eefa9fa48c15
Sugar152 <adebayoabeeb96@gmail.com>
2022-02-17 09:26:34 -0800

    table

---
+++ Hero/abeeb.js
@@ -0,0 +1,94 @@
+console.log(23 + 97)
+    console.log(3 + 4 + 5 + 6 + 7 + 8)
+    console.log((4 + 6 + 9) / 77)
+    let a = 10
+console.log(a)
+9 * a
+console.log(9 * a)
+let b = 12 * a
+// console.log(b)
+
+// let multi = 6* 6
+// console.log(multi)
+// const max = 57
+// console.log(max)
+// const actual  = max - 13
+// console.log(actual)
+// const percentage = actual / max
+// console.log(percentage)
+// let main, hero;
+// main = "Abeeb";
+// hero = main;
+// alert( hero );
+// let x = 5;
+// let y = 7;
+// let z = x + y;
+// console.log(z)
+let x = (100 + 5) * 3  
+console.log(x)
+let t = 30
+let e = 10
+let y = "20"
+let z = t + e + y 
+console.log(z)
+let k = 100
+let h = 10
+let f = k - h
+console.log(f)
+
+let g = NaN;
+console.log(g)
+let u = new Number(123)
+let o = -2 / 0
+console.log(u)
+const myInt = 5;
+const myFloat = 6.667;
+myInt;
+myFloat;
+console.log(myFloat)
+let myNumber = '74';
+myNumber  = +3;
+console.log(myNumber)
+const string = 'The revolution will not be televised.';
+console.log(string);
+const badString1 = "This is a test";
+const badString2 = 'This is a test';
+const badString3 = 'This is a test';
+console.log(badString1)
+const sgl = 'Single quotes.';
+const dbl = "Double quotes";
+console.log(sgl);
+console.log(dbl);
+const sglDbl = 'Would you eat a "fish supper"?';
+const dblSgl = "I'm feeling blue.";
+console.log(sglDbl);
+console.log(dblSgl);
+const badQuotes = "What on earth?";
+console.log(badQuotes)
+const bigmouth = "I've got no right to take my place....";
+console.log(bigmouth)
+// let str = "Apple, Banana, Kiwi";
+// let part = str.slice(-12, -6);
+// console.log(str,part)
+let str = "Apple, Banana, Kiwi";
+let part = str.substring(7, 13);
+console.log(part)
+// let text = "Please visit Microsoft";
+// let newText = text.replace("/MICROSOFT/g", "W3Schools");
+// console.log(newText)
+let text7 = "Hello World!";
+let text8 = text7.toLowerCase();
+console.log(text8)
+let text1 = "Hello" + " " + "World!";
+let text2 = "Hello".concat(" ", "World!");
+
+console.log(text2)
+let text4 = "      Hello World!      ";
+let text5 = text1.trim();
+console.log(text5)
+// let text = "5";
+// let padded = text.padEnd(7,67);
+// console.log(padded)
+let text = "HELLO WORLD";
+let char = text(0);
+console.log(char)
